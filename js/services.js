ed51e5c996ebb2c96bc098c14e82061c63c8782a
upali <upali.choden@selise.ch>
2017-01-20 09:54:30 +0600

    new files

---
+++ js/services.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+
+    angular.module('movieApp.services', []).factory('Movie', function($resource) {
+    return $resource('http://movieapp-sitepointdemos.rhcloud.com/api/movies/:id', { id: '@_id' }, {
+        update: {
+            method: 'PUT'
+        }
+    });
+});
+})();
